bf31f4b06c65ecc84cab505ea8cbba4349a02910
OSLO-support <OSLO-support@vlaanderen.be>
2026-04-22 20:00:32 +0200

    fix: don't show empty column on OVO page

--- components/organization/organization-contact.vue
+++ components/organization/organization-contact.vue
@@ -3,48 +3,46 @@
     <vl-column width="12">
       <vl-title tag-name="h2" mod-h3>Contactgegevens</vl-title>
     </vl-column>
-    <vl-column
-      v-for="contact in contactPoints"
-      :key="contact.id"
-      width="6"
-      width-s="12"
-    >
+    <template v-for="contact in contactPoints" :key="contact.id">
       <!-- if keys are less than 0, it means that there is a contact point with just an ID. That can be ignored -->
-      <vl-info-tile v-if="Object.keys(contact).length > 1">
-        <div slot="content">
-          <p v-if="contact.name">
-            <strong>{{ contact.name }}</strong>
-          </p>
-          <p v-if="contact.email">
-            <vl-icon icon="email" mod-before></vl-icon>
-            <vl-link :href="`mailto:${contact.email}`">{{
-              contact.email
-            }}</vl-link>
-          </p>
-          <p v-if="contact.telephone">
-            <vl-icon icon="phone" mod-before></vl-icon>
-            <vl-link :href="`tel:${contact.telephone}`">{{
-              contact.telephone
-            }}</vl-link>
-          </p>
-          <p v-if="contact.fax">
-            <vl-icon icon="phone" mod-before></vl-icon>
-            <span>Fax: {{ contact.fax }}</span>
-          </p>
-          <p v-if="contact.website">
-            <vl-icon icon="external" mod-before></vl-icon>
-            <vl-link :href="contact.website" external>{{
-              contact.website
-            }}</vl-link>
-          </p>
-          <address v-if="contact.address">
-            <vl-icon icon="location-map" mod-before></vl-icon>
-            {{ contact.address.street }} {{ contact.address.number }}
-            {{ contact.address.postalCode }} {{ contact.address.municipality }}
-          </address>
-        </div>
-      </vl-info-tile>
-    </vl-column>
+      <vl-column v-if="Object.keys(contact).length > 1" width="6" width-s="12">
+        <vl-info-tile>
+          <div slot="content">
+            <p v-if="contact.name">
+              <strong>{{ contact.name }}</strong>
+            </p>
+            <p v-if="contact.email">
+              <vl-icon icon="email" mod-before></vl-icon>
+              <vl-link :href="`mailto:${contact.email}`">{{
+                contact.email
+              }}</vl-link>
+            </p>
+            <p v-if="contact.telephone">
+              <vl-icon icon="phone" mod-before></vl-icon>
+              <vl-link :href="`tel:${contact.telephone}`">{{
+                contact.telephone
+              }}</vl-link>
+            </p>
+            <p v-if="contact.fax">
+              <vl-icon icon="phone" mod-before></vl-icon>
+              <span>Fax: {{ contact.fax }}</span>
+            </p>
+            <p v-if="contact.website">
+              <vl-icon icon="external" mod-before></vl-icon>
+              <vl-link :href="contact.website" external>{{
+                contact.website
+              }}</vl-link>
+            </p>
+            <address v-if="contact.address">
+              <vl-icon icon="location-map" mod-before></vl-icon>
+              {{ contact.address.street }} {{ contact.address.number }}
+              {{ contact.address.postalCode }}
+              {{ contact.address.municipality }}
+            </address>
+          </div>
+        </vl-info-tile>
+      </vl-column>
+    </template>
   </template>
 </template>
 
