40d6f9f13a4153c69383591e2ec0bf98252ac30a
Dylan Van Assche <dylan.vanassche@vlaanderen.be>
2026-06-22 20:28:34 +0200

    feat: add landing pages onderneming and vestiging

---
+++ pages/onderneming/index.vue
@@ -0,0 +1,37 @@
+<template>
+  <content-header
+    title="Onderneming"
+    href="https://www.vlaanderen.be/digitaal-vlaanderen"
+  />
+  <vl-layout>
+    <vl-region>
+      <vl-grid mod-v-center mod-center mod-stacked>
+        <vl-column width="12">
+          <vl-title mod-no-space-bottom tag-name="h1">
+            PURIs Onderneming
+          </vl-title>
+        </vl-column>
+        <vl-column width="12">
+          <div>
+            <p>
+              Op deze pagina vind je de PURIs (Persistente URI) voor ondernemingen zoals opgenomen in de Kruispuntbank van Ondernemingen (KBO).
+              Elke onderneming heeft een unieke en persistente identificator in de vorm van een PURI, conform de Vlaamse URI standaard.
+              Deze PURIs zullen onderhouden worden zodat ze persistent, uniek, en beschikbaar blijven.
+            </p>
+            <p>
+              Deze PURIs maken het mogelijk om ondernemingen op een consistente manier te identificeren
+              en te linken aan ondernemingen en andere relevante datasets.
+              Dit bevordert de kwaliteit, samenhang, interoperabiliteit, en herbruikbaarheid van de data in uiteenlopende toepassingen.
+            </p>
+            <p>
+              De pagina's en bijhorende PURIs vormen een essentieel onderdeel voor wie ondernemingsgegevens wil verwerken en integreren
+              met andere data volgens de OSLO datastandaarden.
+            </p>
+          </div>
+        </vl-column>
+      </vl-grid>
+    </vl-region>
+  </vl-layout>
+
+  <content-footer />
+</template>
---
+++ pages/vestiging/index.vue
@@ -0,0 +1,37 @@
+<template>
+  <content-header
+    title="Vestiging"
+    href="https://www.vlaanderen.be/digitaal-vlaanderen"
+  />
+  <vl-layout>
+    <vl-region>
+      <vl-grid mod-v-center mod-center mod-stacked>
+        <vl-column width="12">
+          <vl-title mod-no-space-bottom tag-name="h1">
+            PURIs Vestiging
+          </vl-title>
+        </vl-column>
+        <vl-column width="12">
+          <div>
+            <p>
+              Op deze pagina vind je de PURIs (Persistente URI) voor vestigingseenheden zoals opgenomen in de Kruispuntbank van Ondernemingen (KBO).
+              Elke vestiging heeft een unieke en persistente identificator in de vorm van een PURI, conform de Vlaamse URI standaard.
+              Deze PURIs zullen onderhouden worden zodat ze persistent, uniek, en beschikbaar blijven.
+            </p>
+            <p>
+              Deze PURIs maken het mogelijk om vestigingen op een consistente manier te identificeren
+              en te linken aan ondernemingen en andere relevante datasets.
+              Dit bevordert de kwaliteit, samenhang en herbruikbaarheid van de data in uiteenlopende toepassingen.
+            </p>
+            <p>
+              De pagina's en bijhorende PURIs vormen een essentieel onderdeel voor wie vestigingsgegevens wil verwerken en integreren
+              met andere data volgens de OSLO datastandaarden.
+            </p>
+          </div>
+        </vl-column>
+      </vl-grid>
+    </vl-region>
+  </vl-layout>
+
+  <content-footer />
+</template>
