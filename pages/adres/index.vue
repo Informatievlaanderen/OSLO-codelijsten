5478ce413f49f637314c52d0e789beb12040d9ec
OSLO-support <OSLO-support@vlaanderen.be>
2026-09-03 15:22:34 +0200

    feat: added generic root page for the GRAR subjectpages

---
+++ pages/adres/index.vue
@@ -0,0 +1,37 @@
+<template>
+  <content-header
+    title="Adres"
+    href="https://www.vlaanderen.be/digitaal-vlaanderen"
+  />
+  <vl-layout>
+    <vl-region>
+      <vl-grid mod-v-center mod-center mod-stacked>
+        <vl-column width="12">
+          <vl-title mod-no-space-bottom tag-name="h1">
+            PURIs Adres
+          </vl-title>
+        </vl-column>
+        <vl-column width="12">
+          <div>
+            <p>
+              Op deze pagina vind je de PURIs (Persistente URI) voor adressen zoals opgenomen in het Vlaamse adressenregister (GRAR).
+              Elk adres heeft een unieke en persistente identificator in de vorm van een PURI, conform de Vlaamse URI standaard.
+              Deze PURIs zullen onderhouden worden zodat ze persistent, uniek, en beschikbaar blijven.
+            </p>
+            <p>
+              Deze PURIs maken het mogelijk om adressen op een consistente manier te identificeren
+              en te linken aan gebouweenheden, percelen en andere relevante datasets.
+              Dit bevordert de kwaliteit, samenhang, interoperabiliteit, en herbruikbaarheid van de data in uiteenlopende toepassingen.
+            </p>
+            <p>
+              De pagina's en bijhorende PURIs vormen een essentieel onderdeel voor wie adresgegevens wil verwerken en integreren
+              met andere data volgens de OSLO datastandaarden.
+            </p>
+          </div>
+        </vl-column>
+      </vl-grid>
+    </vl-region>
+  </vl-layout>
+
+  <content-footer />
+</template>
---
+++ pages/postinfo/index.vue
@@ -0,0 +1,37 @@
+<template>
+  <content-header
+    title="Postinfo"
+    href="https://www.vlaanderen.be/digitaal-vlaanderen"
+  />
+  <vl-layout>
+    <vl-region>
+      <vl-grid mod-v-center mod-center mod-stacked>
+        <vl-column width="12">
+          <vl-title mod-no-space-bottom tag-name="h1">
+            PURIs Postinfo
+          </vl-title>
+        </vl-column>
+        <vl-column width="12">
+          <div>
+            <p>
+              Op deze pagina vind je de PURIs (Persistente URI) voor postinfo zoals opgenomen in het Vlaamse adressenregister (GRAR).
+              Elke postinfo heeft een unieke en persistente identificator in de vorm van een PURI, conform de Vlaamse URI standaard.
+              Deze PURIs zullen onderhouden worden zodat ze persistent, uniek, en beschikbaar blijven.
+            </p>
+            <p>
+              Deze PURIs maken het mogelijk om postinfo op een consistente manier te identificeren
+              en te linken aan gemeenten, adressen en andere relevante datasets.
+              Dit bevordert de kwaliteit, samenhang, interoperabiliteit, en herbruikbaarheid van de data in uiteenlopende toepassingen.
+            </p>
+            <p>
+              De pagina's en bijhorende PURIs vormen een essentieel onderdeel voor wie postinfo wil verwerken en integreren
+              met andere data volgens de OSLO datastandaarden.
+            </p>
+          </div>
+        </vl-column>
+      </vl-grid>
+    </vl-region>
+  </vl-layout>
+
+  <content-footer />
+</template>
